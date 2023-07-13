9e35d9395ca8c4c11ab05ecd4ef03b6c99d588cf
xenedium <ahmed@abderraziq.com>
2023-07-13 13:14:00 +0100

    Linking nodes to control plane

--- provisioning.tf
+++ provisioning.tf
@@ -9,7 +9,8 @@ resource "null_resource" "control_plane_provisioning" {
 
   provisioner "remote-exec" {
     inline = [
-      "curl https://cdn.abderraziq.com/k8s/init.sh | bash -s ${digitalocean_domain.control_plane_domain.name}"
+      "curl https://cdn.abderraziq.com/k8s/init.sh | bash -s ${digitalocean_domain.control_plane_domain.name}",
+      "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > hash.txt"
     ]
   }
 }
@@ -25,7 +26,7 @@ resource "null_resource" "node_one_provisioning" {
 
   provisioner "remote-exec" {
     inline = [
-      "curl https://cdn.abderraziq.com/k8s/init-node.sh | bash"
+      "curl https://cdn.abderraziq.com/k8s/init-node.sh | bash -s ${digitalocean_domain.control_plane_domain.name}:6443 ${data.external.kube_token.result} ${data.external.kube_token_hash.result}"
     ]
   }
 }
@@ -41,8 +42,17 @@ resource "null_resource" "node_two_provisioning" {
 
   provisioner "remote-exec" {
     inline = [
-      "curl https://cdn.abderraziq.com/k8s/init-node.sh | bash"
+      "curl https://cdn.abderraziq.com/k8s/init-node.sh | bash -s -s ${digitalocean_domain.control_plane_domain.name}:6443 ${data.external.kube_token.result} ${data.external.kube_token_hash.result}"
     ]
   }
 }
 
+data "external" "kube_token" {
+  program    = ["sh", "-c", "ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${digitalocean_domain.control_plane_domain.name} 'kubeadm token create'"]
+  depends_on = [null_resource.control_plane_provisioning]
+}
+
+data "external" "kube_token_hash" {
+  program    = ["sh", "-c", "ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${digitalocean_domain.control_plane_domain.name} 'cat hash.txt'"]
+  depends_on = [null_resource.control_plane_provisioning]
+}
