97ec45d5c0e7a8b89a5aa9ba4287d472e88fe16f
xenedium <ahmed@abderraziq.com>
2023-07-29 20:22:33 +0100

    MLB deployment

--- provisioning.tf
+++ provisioning.tf
@@ -13,8 +13,7 @@ resource "null_resource" "control_plane_provisioning" {
       "curl https://cdn.abderraziq.com/k8s/init.sh | bash -s ${digitalocean_domain.control_plane_domain.name}",
       "sed -i 's/0.0.0.0/${digitalocean_droplet.control_plane.ipv4_address}/g' mlb.yaml",
       "sed -i 's/1.1.1.1/${digitalocean_droplet.node_one.ipv4_address}/g' mlb.yaml",
-      "sed -i 's/2.2.2.2/${digitalocean_droplet.node_two.ipv4_address}/g' mlb.yaml",
-      "KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f mlb.yaml"
+      "sed -i 's/2.2.2.2/${digitalocean_droplet.node_two.ipv4_address}/g' mlb.yaml"
     ]
   }
 }
@@ -59,4 +58,9 @@ data "external" "kube_token" {
 data "external" "kube_token_hash" {
   program    = ["sh", "-c", "ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${digitalocean_domain.control_plane_domain.name} 'curl https://cdn.abderraziq.com/k8s/get-token-hash.sh | bash'"]
   depends_on = [null_resource.control_plane_provisioning]
+}
+
+data "external" "apply_mlb" {
+  program    = ["sh", "-c", "ssh -i ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${digitalocean_domain.control_plane_domain.name} 'KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f mlb.yaml'"]
+  depends_on = [null_resource.node_one_provisioning, null_resource.node_two_provisioning]
 }
